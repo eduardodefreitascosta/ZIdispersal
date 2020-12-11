cf3e2dff2a1891411482a5c23487e44cd075cd05
Eduardo de Freitas Costa <60922090+eduardodefreitascosta@users.noreply.github.com>
2020-12-11 01:07:21 +0100

    general function

--- ZIgamma.R
+++ ZIgamma.R
@@ -1,9 +1,6 @@
 
 
-ZIgamma<-function(ze,t,d,data,co=c()){
-
-
-  data_log<-data[, c(co)]
+ZIgamma<-function(ze,t,d,data,co=c(),dist){
 
   #Packages to be used
   packages<-c("flexsurv","survival","here")
@@ -18,23 +15,33 @@ ZIgamma<-function(ze,t,d,data,co=c()){
   # Packages loading
   invisible(lapply(packages, library, character.only = TRUE))
   
+  
+  attach(data)
+
+if(dist=="gamma"){
+  
   if(length(co)!=0){
+    
+      data_log<-cbind.data.frame(ze,data[, c(co)])
+    
+  
   a<-glm(ze~.,family=binomial(link='logit'),data=data_log)
 
   (fmla <- as.formula(paste("Surv(t,d,type='right') ~ ", paste(co, collapse= "+"))))
 
   data_gamma<-(cbind.data.frame(t,d, data_log))
 
-  b<-flexsurvreg(fmla,data=data_gamma,subset=t!=0,dist="gamma")
+  b<-flexsurvreg(fmla,data=data_gamma,subset=t!=0,dist=dist)
   
   }else{
-    a<-glm(ze~1,family=binomial(link='logit'),data=data_log)
+    
+    
+    a<-glm(ze~1,family=binomial(link='logit'))
     
     (fmla <- paste("Surv(t,d,type='right') ~ 1"))
     
-    data_gamma<-(cbind.data.frame(t,d, data_log))
-    
-    b<-flexsurvreg(fmla,data=data_gamma,subset=t!=0,dist="gamma")
+      
+    b<-flexsurvreg(Surv(subset(t,t>0),subset(d,t>0),type='right') ~ 1,dist=dist)
    
   }
 
@@ -42,12 +49,12 @@ ZIgamma<-function(ze,t,d,data,co=c()){
   summa<-list(a,b)
   names(summa)<-c("Logistic","Gamma")
 
-
-  return(summa)
-
+detach(data,unload = T)
+   return(summa)
+}else warning('So far, only gamma distribution is possible: "gamma" ')
 
 }
 
 
 
-ZIgamma(ze=zero,t=dist,d=delta,data=wild_boar,co=c("age","sex"))
+ZIgamma(ze=zero,t=dist,d=delta,data=wild_boar,co=c("age"),dist="weibull")
