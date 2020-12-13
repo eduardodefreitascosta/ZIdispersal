1280f2825a7108ec1d1e5763d62ad5e7be269a7b
Eduardo de Freitas Costa <60922090+eduardodefreitascosta@users.noreply.github.com>
2020-12-13 10:29:54 +0100

    outputs in weibull

--- ZIdispersal/R/Ziweibull.R
+++ ZIdispersal/R/Ziweibull.R
@@ -16,7 +16,7 @@ ZIweibull<-function(t,d,data,co=c(),dist){
           a       =   exp(varp[1])
           lam     =   exp(varp[2])
 
-          gamm_zer = (exp(varp[3]))/(1+exp(varp[4]))
+          gamm_zer = (exp(varp[3]))/(1+exp(varp[3]))
 
           f0   = gamm_zer
           fw = (exp(log(a)-log(lam)))*((exp(log(t)-log(lam)))^(a-1))*(exp(-(exp(log(t)-log(lam)))^a))
@@ -32,7 +32,7 @@ ZIweibull<-function(t,d,data,co=c(),dist){
 
 
 # initial value
-varp=rep(0,9)
+varp=rep(0,3)
 
 fit=try(optim(varp,GPE,method="BFGS",hessian=TRUE,control=list(fnscale=-1)))
 
@@ -40,7 +40,36 @@ estc = try(fit$par)
 Hc=try(fit$hessian); varic=try(-solve(Hc))
 
 
-cbind(estc,sqrt(diag(varic)),estc-1.96*sqrt(diag(varic)),estc+1.96*sqrt(diag(varic)))
+
+#Hypothesis test weibull
+betas_weibull=estc
+SE_weibull=sqrt(diag(varic))
+wald_weibull=betas_weibull^2/SE_weibull^2
+
+
+p_val<-numeric()
+for (i in 1:length(fit$par)){
+if(pchisq(wald_weibull[i],1,lower.tail = F)<0.001){
+  p_val[i]<-"<0.001"
+}else p_val[i]<-pchisq(wald_weibull[i],1,lower.tail = F)
+}
+
+
+AIC=(2*length(fit$par))-2*fit$value
+AICc=AIC-(2*length(fit$par)*(length(fit$par)+1)/(length(t)-length(fit$par)-1))
+BIC=(log(length(t))*length(fit$par))-2*fit$value
+
+main<-cbind.data.frame(estc,sqrt(diag(varic)),p_val)
+colnames(main)<-c("Estimates","Std Err","P-value")
+rownames(main)<-c("Shape",co,"Scale",co,"Intercept",co)
+
+cal<- (paste("ZIweibull:t ~ "))
+
+
+out<-list(cal,main,fit$value,length(t),sum(t==0),sum(d==0),length(fit$par)-1,AIC,AICc,BIC,wild_boar,fit$hessian)
+names(out)<-c("Call","Coefficients","LogLik","N","Zeros","Censored","df","AIC","AICc","BIC","Data","Hessian")
+
+return(out)
 
 
   }else{
@@ -83,7 +112,35 @@ cbind(estc,sqrt(diag(varic)),estc-1.96*sqrt(diag(varic)),estc+1.96*sqrt(diag(var
       Hc=try(fit$hessian); varic=try(-solve(Hc))
 
 
-      cbind(estc,sqrt(diag(varic)),estc-1.96*sqrt(diag(varic)),estc+1.96*sqrt(diag(varic)))
+
+      #Hypothesis test weibull
+      betas_weibull=estc
+      SE_weibull=sqrt(diag(varic))
+      wald_weibull=betas_weibull^2/SE_weibull^2
+
+
+      p_val<-numeric()
+      for (i in 1:length(fit$par)){
+        if(pchisq(wald_weibull[i],1,lower.tail = F)<0.001){
+          p_val[i]<-"<0.001"
+        }else p_val[i]<-pchisq(wald_weibull[i],1,lower.tail = F)
+
+        }
+
+
+      AIC=(2*length(fit$par))-2*fit$value
+      AICc=AIC-(2*length(fit$par)*(length(fit$par)+1)/(length(t)-length(fit$par)-1))
+      BIC=(log(length(t))*length(fit$par))-2*fit$value
+
+      main<-cbind.data.frame(estc,sqrt(diag(varic)),p_val)
+      colnames(main)<-c("Estimates","Std Err","P-value")
+      rownames(main)<-c("Shape",paste("Shape", co),"Scale",paste("Scale",co),"Intercept",paste("Intercept",co))
+      cal<- as.formula(paste("ZIweibull:t ~ ", paste(co, collapse= "+")))
+
+      out<-list(cal,main,fit$value,length(t),sum(t==0),sum(d==0),length(fit$par)-1,AIC,AICc,BIC,wild_boar,fit$hessian)
+      names(out)<-c("Call","Coefficients","LogLik","N","Zeros","Censored","df","AIC","AICc","BIC","Data","Hessian")
+
+      return(out)
 }else warning('Currently only two variables are possible')
 
 }
